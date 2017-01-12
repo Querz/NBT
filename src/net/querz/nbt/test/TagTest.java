0c9716f4ec67c7ad1e131348693a871a6acc7278
rb <info@querz.de>
2017-01-12 14:22:05 +0100

    add static import for TestUtil

--- src/net/querz/nbt/test/TagTest.java
+++ src/net/querz/nbt/test/TagTest.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
+import static net.querz.nbt.test.TestUtil.*;
 
 import junit.framework.TestCase;
 import net.querz.nbt.*;
@@ -116,7 +117,7 @@ public class TagTest extends TestCase {
 		assertTrue(byteList2.getBoolean(2));
 		assertFalse(byteList2.getBoolean(3));
 		assertEquals(byteList2.asByte(0), byteList.getByte(0));
-		TestUtil.assertThrowsException(() -> byteList.add(maxShort), IllegalArgumentException.class);
+		assertThrowsException(() -> byteList.add(maxShort), IllegalArgumentException.class);
 	}
 	
 	public void testCompoundTag() throws IOException {
--- src/net/querz/nbt/test/TagTypeTest.java
+++ src/net/querz/nbt/test/TagTypeTest.java
@@ -3,14 +3,15 @@ package net.querz.nbt.test;
 import junit.framework.TestCase;
 import net.querz.nbt.TagType;
 import net.querz.nbt.custom.ShortArrayTag;
+import static net.querz.nbt.test.TestUtil.*;
 
 public class TagTypeTest extends TestCase {
 	public void testExceptions() {
-		TestUtil.assertThrowsException(() -> TagType.getCustomTag(-1), IllegalArgumentException.class);
-		TestUtil.assertThrowsException(() -> TagType.CUSTOM.getTag(), IllegalArgumentException.class);
-		TestUtil.assertThrowsException(() -> TagType.match(-1), IllegalArgumentException.class);
+		assertThrowsException(() -> TagType.getCustomTag(-1), IllegalArgumentException.class);
+		assertThrowsException(() -> TagType.CUSTOM.getTag(), IllegalArgumentException.class);
+		assertThrowsException(() -> TagType.match(-1), IllegalArgumentException.class);
 		TagType.registerCustomTag(100, ShortArrayTag.class);
-		TestUtil.assertThrowsException(() -> TagType.registerCustomTag(100, ShortArrayTag.class), IllegalArgumentException.class);
+		assertThrowsException(() -> TagType.registerCustomTag(100, ShortArrayTag.class), IllegalArgumentException.class);
 	}
 	
 	public void testCustomTags() {
