7cd8d13d3d5d1f9b7e6c13eb7273f9feafd2085f
Marcono1234 <Marcono1234@users.noreply.github.com>
2019-01-28 15:29:31 +0100

    Suppressed warning and added small doc

--- src/main/java/net/querz/nbt/MaxDepthReachedException.java
+++ src/main/java/net/querz/nbt/MaxDepthReachedException.java
@@ -1,7 +1,10 @@
 package net.querz.nbt;
 
+/**
+ * Exception indicating that the maximum (de-)serialization depth has been reached.
+ */
+@SuppressWarnings("serial")
 public class MaxDepthReachedException extends RuntimeException {
-
 	public MaxDepthReachedException(String msg) {
 		super(msg);
 	}
