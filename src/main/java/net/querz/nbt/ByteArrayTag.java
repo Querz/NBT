a7bc3c99d85fd1e1fb5f9ee4c62cbdb720d9fe3b
Marcono1234 <Marcono1234@users.noreply.github.com>
2019-01-28 15:28:49 +0100

    Removed unused import

--- src/main/java/net/querz/nbt/ByteArrayTag.java
+++ src/main/java/net/querz/nbt/ByteArrayTag.java
@@ -3,7 +3,6 @@ package net.querz.nbt;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class ByteArrayTag extends ArrayTag<byte[]> implements Comparable<ByteArrayTag> {
