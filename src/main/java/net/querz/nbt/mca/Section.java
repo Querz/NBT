badcf2dd66d682420ba553bfddc55875363c1773
Querz <info@querz.net>
2019-09-26 17:24:35 +0200

    add javadoc to mca library and make some functions public

--- src/main/java/net/querz/nbt/mca/Section.java
+++ src/main/java/net/querz/nbt/mca/Section.java
@@ -16,6 +16,10 @@ public class Section {
 	private long[] blockStates;
 	private byte[] skyLight;
 
+	/**
+	 * Creates a new Section based on raw section data.
+	 * @param sectionRoot The raw section data
+	 */
 	public Section(CompoundTag sectionRoot) {
 		ListTag<?> rawPalette = sectionRoot.getListTag("Palette");
 		if (rawPalette == null) {
@@ -34,7 +38,7 @@ public class Section {
 
 	Section() {}
 
-	void putValueIndexedPalette(CompoundTag data, int index) {
+	private void putValueIndexedPalette(CompoundTag data, int index) {
 		PaletteIndex leaf = new PaletteIndex(data, index);
 		String name = data.getString("Name");
 		List<PaletteIndex> leaves = valueIndexedPalette.get(name);
@@ -52,7 +56,7 @@ public class Section {
 		}
 	}
 
-	PaletteIndex getValueIndexedPalette(CompoundTag data) {
+	private PaletteIndex getValueIndexedPalette(CompoundTag data) {
 		List<PaletteIndex> leaves = valueIndexedPalette.get(data.getString("Name"));
 		if (leaves == null) {
 			return null;
@@ -76,16 +80,38 @@ public class Section {
 		}
 	}
 
+	/**
+	 * Checks whether the data of this Section is empty.
+	 * @return true if empty
+	 */
 	public boolean isEmpty() {
 		return data == null;
 	}
 
+	/**
+	 * Fetches a block state based on a block location from this section.
+	 * The coordinates represent the location of the block inside of this Section.
+	 * @param blockX The x-coordinate of the block in this Section
+	 * @param blockY The y-coordinate of the block in this Section
+	 * @param blockZ The z-coordinate of the block in this Section
+	 * @return The block state data of this block.
+	 */
 	public CompoundTag getBlockStateAt(int blockX, int blockY, int blockZ) {
 		int index = getBlockIndex(blockX, blockY, blockZ);
 		int paletteIndex = getPaletteIndex(index);
 		return palette.get(paletteIndex);
 	}
 
+	/**
+	 * Attempts to add a block state for a specific block location in this Section.
+	 * @param blockX The x-coordinate of the block in this Section
+	 * @param blockY The y-coordinate of the block in this Section
+	 * @param blockZ The z-coordinate of the block in this Section
+	 * @param state The block state to be set
+	 * @param cleanup When <code>true</code>, it will cleanup the palette of this section.
+	 *                This option should only be used moderately to avoid unnecessary recalculation of the palette indices.
+	 *                Recalculating the Palette should only be executed once right before saving the Section to file.
+	 */
 	public void setBlockStateAt(int blockX, int blockY, int blockZ, CompoundTag state, boolean cleanup) {
 		int paletteSizeBefore = palette.size();
 		int paletteIndex = addToPalette(state);
@@ -142,7 +168,11 @@ public class Section {
 		}
 	}
 
-	ListTag<CompoundTag> getPalette() {
+	/**
+	 * Fetches the palette of this Section.
+	 * @return The palette of this Section.
+	 */
+	public ListTag<CompoundTag> getPalette() {
 		return palette;
 	}
 
@@ -156,27 +186,32 @@ public class Section {
 		return palette.size() - 1;
 	}
 
-	int getBlockIndex(int blockX, int blockY, int blockZ) {
+	private int getBlockIndex(int blockX, int blockY, int blockZ) {
 		return (blockY & 0xF) * 256 + (blockZ & 0xF) * 16 + (blockX & 0xF);
 	}
 
-	static long updateBits(long n, long m, int i, int j) {
+	private static long updateBits(long n, long m, int i, int j) {
 		//replace i to j in n with j - i bits of m
 		long mShifted = i > 0 ? (m & ((1L << j - i) - 1)) << i : (m & ((1L << j - i) - 1)) >>> -i;
 		return ((n & ((j > 63 ? 0 : (~0L << j)) | (i < 0 ? 0 : ((1L << i) - 1L)))) | mShifted);
 	}
 
-	static long bitRange(long value, int from, int to) {
+	private static long bitRange(long value, int from, int to) {
 		int waste = 64 - to;
 		return (value << waste) >>> (waste + from);
 	}
 
+	/**
+	 * This method recalculates the palette and its indices.
+	 * This should only be used moderately to avoid unnecessary recalculation of the palette indices.
+	 * Recalculating the Palette should only be executed once right before saving the Section to file.
+	 */
 	public void cleanupPaletteAndBlockStates() {
 		Map<Integer, Integer> oldToNewMapping = cleanupPalette();
 		adjustBlockStateBits(oldToNewMapping, blockStates);
 	}
 
-	Map<Integer, Integer> cleanupPalette() {
+	private Map<Integer, Integer> cleanupPalette() {
 		//create index - palette mapping
 		Map<Integer, Integer> allIndices = new HashMap<>();
 		for (int i = 0; i < 4096; i++) {
@@ -202,7 +237,7 @@ public class Section {
 		return allIndices;
 	}
 
-	void adjustBlockStateBits(Map<Integer, Integer> oldToNewMapping, long[] blockStates) {
+	private void adjustBlockStateBits(Map<Integer, Integer> oldToNewMapping, long[] blockStates) {
 		//increases or decreases the amount of bits used per BlockState
 		//based on the size of the palette. oldToNewMapping can be used to update indices
 		//if the palette had been cleaned up before using MCAFile#cleanupPalette().
@@ -223,10 +258,18 @@ public class Section {
 		this.blockStates = newBlockStates;
 	}
 
+	/**
+	 * @return The block light array of this Section
+	 */
 	public byte[] getBlockLight() {
 		return blockLight;
 	}
 
+	/**
+	 * Sets the block light array for this section.
+	 * @param blockLight The block light array
+	 * @throws IllegalArgumentException When the length of the array is not 2048
+	 */
 	public void setBlockLight(byte[] blockLight) {
 		if (blockLight != null && blockLight.length != 2048) {
 			throw new IllegalArgumentException("BlockLight array must have a length of 2048");
@@ -234,10 +277,19 @@ public class Section {
 		this.blockLight = blockLight;
 	}
 
+	/**
+	 * @return The indices of the block states of this Section.
+	 */
 	public long[] getBlockStates() {
 		return blockStates;
 	}
 
+	/**
+	 * Sets the block state indices to a custom value.
+	 * @param blockStates The block state indices.
+	 * @throws NullPointerException If <code>blockStates</code> is <code>null</code>
+	 * @throws IllegalArgumentException When <code>blockStates</code>' length is &lt; 256 or &gt; 4096 and is not a multiple of 64
+	 */
 	public void setBlockStates(long[] blockStates) {
 		if (blockStates == null) {
 			throw new NullPointerException("BlockStates cannot be null");
@@ -247,10 +299,18 @@ public class Section {
 		this.blockStates = blockStates;
 	}
 
+	/**
+	 * @return The sky light values of this Section
+	 */
 	public byte[] getSkyLight() {
 		return skyLight;
 	}
 
+	/**
+	 * Sets the sky light values of this section.
+	 * @param skyLight The custom sky light values
+	 * @throws IllegalArgumentException If the length of the array is not 2048
+	 */
 	public void setSkyLight(byte[] skyLight) {
 		if (skyLight != null && skyLight.length != 2048) {
 			throw new IllegalArgumentException("SkyLight array must have a length of 2048");
@@ -258,6 +318,10 @@ public class Section {
 		this.skyLight = skyLight;
 	}
 
+	/**
+	 * Creates an empty Section with base values.
+	 * @return An empty Section
+	 */
 	public static Section newSection() {
 		Section s = new Section();
 		s.blockStates = new long[256];
@@ -269,6 +333,13 @@ public class Section {
 		return s;
 	}
 
+	/**
+	 * Updates the raw CompoundTag that this Section is based on.
+	 * This must be called before saving a Section to disk if the Section was manually created
+	 * to set the Y of this Section.
+	 * @param y The Y-value of this Section
+	 * @return A reference to the raw CompoundTag this Section is based on
+	 */
 	public CompoundTag updateHandle(int y) {
 		data.putByte("Y", (byte) y);
 		data.put("Palette", palette);
