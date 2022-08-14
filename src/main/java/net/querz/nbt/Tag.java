9d6ed9c30a1868307fc57b208840e4c32b21b32a
HoldYourWaffle <ravivanrooijen@live.nl>
2022-08-14 23:42:32 +0200

    Add tag class to Tag.Type

--- src/main/java/net/querz/nbt/Tag.java
+++ src/main/java/net/querz/nbt/Tag.java
@@ -8,26 +8,28 @@ public sealed interface Tag permits CollectionTag, CompoundTag, EndTag, NumberTa
 	int MAX_DEPTH = 512;
 
 	enum Type {
-		END(0, EndTag.READER, false),
-		BYTE(1, ByteTag.READER, true),
-		SHORT(2, ShortTag.READER, true),
-		INT(3, IntTag.READER, true),
-		LONG(4, LongTag.READER, true),
-		FLOAT(5, FloatTag.READER, true),
-		DOUBLE(6, DoubleTag.READER, true),
-		BYTE_ARRAY(7, ByteArrayTag.READER, false),
-		STRING(8, StringTag.READER, false),
-		LIST(9, ListTag.READER, false),
-		COMPOUND(10, CompoundTag.READER, false),
-		INT_ARRAY(11, IntArrayTag.READER, false),
-		LONG_ARRAY(12, LongArrayTag.READER, false);
+		END(0, EndTag.class, EndTag.READER, false),
+		BYTE(1, ByteTag.class, ByteTag.READER, true),
+		SHORT(2, ShortTag.class, ShortTag.READER, true),
+		INT(3, IntTag.class, IntTag.READER, true),
+		LONG(4, LongTag.class, LongTag.READER, true),
+		FLOAT(5, FloatTag.class, FloatTag.READER, true),
+		DOUBLE(6, DoubleTag.class, DoubleTag.READER, true),
+		BYTE_ARRAY(7, ByteArrayTag.class, ByteArrayTag.READER, false),
+		STRING(8, StringTag.class, StringTag.READER, false),
+		LIST(9, ListTag.class, ListTag.READER, false),
+		COMPOUND(10, CompoundTag.class, CompoundTag.READER, false),
+		INT_ARRAY(11, IntArrayTag.class, IntArrayTag.READER, false),
+		LONG_ARRAY(12, LongArrayTag.class, LongArrayTag.READER, false);
 
 		public final byte id;
+		public final Class<? extends Tag> tagClass;
 		public final TagReader<?> reader;
 		public final boolean isNumber;
 
-		Type(int id, TagReader<?> reader, boolean isNumber) {
+		Type(int id, Class<? extends Tag> tagClass, TagReader<?> reader, boolean isNumber) {
 			this.id = (byte) id;
+			this.tagClass = tagClass;
 			this.reader = reader;
 			this.isNumber = isNumber;
 		}
