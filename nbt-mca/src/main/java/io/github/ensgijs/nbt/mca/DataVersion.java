e78d5d47027eaf6a260a6e9fd32982d6d71e90a7
ens-gijs <ens.gijs@gmail.com>
2026-07-29 16:36:51 -0400

    Adding DataVersion JAVA_26_3_0_SNAPSHOT5, JAVA_26_3_0_SNAPSHOT6

--- nbt-mca/src/main/java/io/github/ensgijs/nbt/mca/DataVersion.java
+++ nbt-mca/src/main/java/io/github/ensgijs/nbt/mca/DataVersion.java
@@ -968,6 +968,8 @@ public enum DataVersion {
     JAVA_26_3_0_SNAPSHOT2(4999, 26, 3, 0, "SNAPSHOT-2"),
     JAVA_26_3_0_SNAPSHOT3(5001, 26, 3, 0, "SNAPSHOT-3"),
     JAVA_26_3_0_SNAPSHOT4(5003, 26, 3, 0, "SNAPSHOT-4"),
+    JAVA_26_3_0_SNAPSHOT5(5004, 26, 3, 0, "SNAPSHOT-5"),
+    JAVA_26_3_0_SNAPSHOT6(5005, 26, 3, 0, "SNAPSHOT-6"),
     ;
 
     private static final int[] ids;
